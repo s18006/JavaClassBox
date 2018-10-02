65ccbdf16ffd09f4442405c5fbfe3a3a0da2389a
Kerekes Csaba <s18006@std.it-college.ac.jp>
2018-10-02 21:34:17 +0000

    kadai1002

---
+++ 1002/Helloloop.java
@@ -0,0 +1,13 @@
+import static java.lang.System.*;
+
+public class Helloloop {
+	public static void main(String[] args){
+
+		for(int i = 1; i <= 5; i++) {
+			String space = i * " ";
+			out.println(space + "Hello, World!");
+
+		}
+
+	}
+}
